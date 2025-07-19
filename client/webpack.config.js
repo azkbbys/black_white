304d1bbb3be22bd2e944f3a473aadc86987677d4
azkbbys <oyroyroyr@163.com>
2025-07-19 15:09:57 +0800

    first commit

---
+++ client/webpack.config.js
@@ -0,0 +1,18 @@
+//@ts-ignore
+const path = require("path");
+
+/** @type import('webpack').Configuration */
+const webpackConfig = {
+  resolve: {
+    /**
+     * 本配置以当前文件夹（client）为基准目录。
+     * 别名配置直接指向源代码目录/文件。
+     */
+
+    alias: {
+      "@": path.resolve(__dirname, "src"), // 指向于 client/src 目录
+      "@shares": path.resolve(__dirname, "../shares"), // 指向于 shares 目录
+    },
+  },
+};
+module.exports = webpackConfig;
